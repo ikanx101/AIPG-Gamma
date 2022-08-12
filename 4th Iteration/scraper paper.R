854c3d1ea8002875db277810a7e22ded373202d2
ikanx101 <ikanx101@gmail.com>
2022-08-13 06:26:06 +0700

    scraper

---
+++ 4th Iteration/scraper paper.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+
+library(rvest)
+library(dplyr)
+
+load("semua link.rda")
+
+# function scraper paper
+ambilin = function(tes){
+  df = 
+    tes %>% 
+    read_html() %>% {tibble(
+      judul = html_nodes(.,".content-title") %>% html_text(trim = T),
+      penulis = html_nodes(.,".search-result-authors") %>% html_text(trim = T),
+      tahun = html_nodes(.,".search-result-authors+ .pub-meta-item") %>% html_text(trim = T)
+    )}
+  df$link = tes
+  return(df)
+}
+
+output = vector("list",length(link_publikasi))
+
+for(i in 1:length(link_publikasi)){
+  output[[i]] = ambilin(link_publikasi[i])
+  print(i)
+  time = runif(1,1,5)
+  Sys.sleep(time)
+}
